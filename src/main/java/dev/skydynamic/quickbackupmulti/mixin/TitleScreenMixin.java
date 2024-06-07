97bdb45e7ab85dea54d9707a4ee890921c6589b1
SkyDynamic <tcplxw@outlook.com>
2024-06-07 16:22:46 +0800

    使用多线程制作备份(#28 #31)

--- src/main/java/dev/skydynamic/quickbackupmulti/mixin/TitleScreenMixin.java
+++ src/main/java/dev/skydynamic/quickbackupmulti/mixin/TitleScreenMixin.java
@@ -11,7 +11,6 @@ import net.minecraft.client.gui.tooltip.Tooltip;
 import net.minecraft.client.gui.widget.ButtonWidget;
 
 import net.minecraft.client.gui.widget.ClickableWidget;
-import net.minecraft.text.MutableText;
 import net.minecraft.text.TranslatableTextContent;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
